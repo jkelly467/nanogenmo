a7878cfd49dd45393933c5765072b27919186ede
Jon Kelly <jkelly@adc4gis.com>
2013-11-04 20:52:12 -0600

    add twitter package

--- gen.js
+++ gen.js
@@ -1,6 +1,28 @@
 var markov = require('markov')
 var fs = require('fs')
+var Twit = require('twit')
 var N = {}
+
+var T = new Twit(require('./private/twitter.js')
+
+N.Sources = {
+  horror: [
+    'pg11074.txt',
+    'pg1188.txt',
+    'pg2147.txt',
+    'pg2148.txt',
+    'pg2149.txt',
+    'pg2150.txt',
+    'pg2151.txt',
+    'pg31469.txt',
+    'pg345.txt',
+    'pg375.txt',
+    'pg389.txt',
+    'pg42.txt',
+    'pg5200.txt',
+    'pg7849.txt'
+  ]
+}
 
 N.Rand = {
 	_s0: 0,
